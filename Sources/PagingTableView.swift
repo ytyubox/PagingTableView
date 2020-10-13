43eb1973bcd4ea4ba27719c6bd8f6895e7d39b7e
Tsungyu Yu <42088295+ytyubox@users.noreply.github.com>
2020-10-13 15:43:02 +0800

    Update PagingTableView.swift

--- Sources/PagingTableView.swift
+++ Sources/PagingTableView.swift
@@ -9,86 +9,97 @@
 import UIKit
 
 open class PagingTableView: UITableView {
-
-  private var loadingView: UIView!
-  private var indicator: UIActivityIndicatorView!
-  internal var page: Int = 0
-  internal var previousItemCount: Int = 0
-
-  open var currentPage: Int {
-    get {
-      return page
+    
+    var indicator: UIActivityIndicatorView!
+    private var _tempFooterView: UIView?
+    internal var page: Int = 0
+    internal var previousItemCount: Int = 0
+    internal var previousPageCount = 0
+    
+    open var currentPage: Int {
+        get {
+            return page
+        }
     }
-  }
-
-  open weak var pagingDelegate: PagingTableViewDelegate? {
-    didSet {
-      pagingDelegate?.paginate(self, to: page)
+    
+    open weak var pagingDelegate: PagingTableViewDelegate? {
+        didSet {
+            pagingDelegate?.paginate(self, to: page)
+        }
     }
-  }
-
-  open var isLoading: Bool = false {
-    didSet {
-      isLoading ? showLoading() : hideLoading()
+    
+    open private(set) var isLoading: Bool = false
+    func startLoading() {
+        showLoading()
+        self.isLoading = true
     }
-  }
-
-  open func reset() {
-    page = 0
-    previousItemCount = 0
-    pagingDelegate?.paginate(self, to: page)
-  }
-
-  private func paginate(_ tableView: PagingTableView, forIndexAt indexPath: IndexPath) {
-    let itemCount = tableView.dataSource?.tableView(tableView, numberOfRowsInSection: indexPath.section) ?? 0
-    guard indexPath.row == itemCount - 1 else { return }
-    guard previousItemCount != itemCount else { return }
-    page += 1
-    previousItemCount = itemCount
-    pagingDelegate?.paginate(self, to: page)
-  }
-
-  private func showLoading() {
-    if loadingView == nil {
-      createLoadingView()
+    func stopLoading(insertSectionWithRowCount count:Int, with rowAnimation: UITableView.RowAnimation = .none) {
+        self.isLoading = false
+        hideLoading()
+        beginUpdates()
+        insertSections(IndexSet(page...page), with: rowAnimation)
+        insertRows(at: (0..<count).map{IndexPath(row: $0, section: page)}, with: rowAnimation)
+        endUpdates()
     }
-    tableFooterView = loadingView
-  }
-
-  private func hideLoading() {
-    reloadData()
-    pagingDelegate?.didPaginate?(self, to: page)
-    tableFooterView = nil
-  }
-
-  private func createLoadingView() {
-    loadingView = UIView(frame: CGRect(x: 0, y: 0, width: frame.width, height: 50))
-    indicator = UIActivityIndicatorView()
-    indicator.color = UIColor.lightGray
-    indicator.translatesAutoresizingMaskIntoConstraints = false
-    indicator.startAnimating()
-    loadingView.addSubview(indicator)
-    centerIndicator()
-    tableFooterView = loadingView
-  }
-
-  private func centerIndicator() {
-    let xCenterConstraint = NSLayoutConstraint(
-      item: loadingView, attribute: .centerX, relatedBy: .equal,
-      toItem: indicator, attribute: .centerX, multiplier: 1, constant: 0
-    )
-    loadingView.addConstraint(xCenterConstraint)
-
-    let yCenterConstraint = NSLayoutConstraint(
-      item: loadingView, attribute: .centerY, relatedBy: .equal,
-      toItem: indicator, attribute: .centerY, multiplier: 1, constant: 0
-    )
-    loadingView.addConstraint(yCenterConstraint)
-  }
-
-  override open func dequeueReusableCell(withIdentifier identifier: String, for indexPath: IndexPath) -> UITableViewCell {
-    paginate(self, forIndexAt: indexPath)
-    return super.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
-  }
-
+    
+    open func reset() {
+        page = 0
+        previousItemCount = 0
+        pagingDelegate?.paginate(self, to: page)
+    }
+    
+    private func paginate(forIndexAt indexPath: IndexPath) {
+        guard
+            let totalSectionCount = dataSource?.numberOfSections?(in: self),
+            let totalItemCount = dataSource?.tableView(self, numberOfRowsInSection: indexPath.section),
+            indexPath.row == totalItemCount - 1,
+            indexPath.section == totalSectionCount - 1 else { return }
+        page += 1
+        previousItemCount = totalItemCount
+        pagingDelegate?.paginate(self, to: page)
+    }
+    
+    private func showLoading() {
+        _tempFooterView = tableFooterView
+        tableFooterView = createLoadingView()
+        indicator.startAnimating()
+    }
+    
+    private func hideLoading() {
+        indicator.stopAnimating()
+        pagingDelegate?.didPaginate(self, to: page)
+        self.tableFooterView = _tempFooterView
+        _tempFooterView = nil
+    }
+    
+    private func createLoadingView() -> UIView {
+        let loadingView = UIView(frame: CGRect(x: 0, y: 0, width: frame.width, height: 50))
+        indicator = UIActivityIndicatorView()
+        indicator.color = UIColor.lightGray
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        indicator.startAnimating()
+        loadingView.addSubview(indicator)
+        func centerIndicator() {
+            let xCenterConstraint = NSLayoutConstraint(
+                item: loadingView, attribute: .centerX, relatedBy: .equal,
+                toItem: indicator, attribute: .centerX, multiplier: 1, constant: 0
+            )
+            loadingView.addConstraint(xCenterConstraint)
+            
+            let yCenterConstraint = NSLayoutConstraint(
+                item: loadingView, attribute: .centerY, relatedBy: .equal,
+                toItem: indicator, attribute: .centerY, multiplier: 1, constant: 0
+            )
+            loadingView.addConstraint(yCenterConstraint)
+        }
+        centerIndicator()
+        return loadingView
+    }
+    
+    
+    override open func dequeueReusableCell(withIdentifier identifier: String, for indexPath: IndexPath) -> UITableViewCell {
+        paginate(forIndexAt: indexPath)
+        return super.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+    }
+    
 }
